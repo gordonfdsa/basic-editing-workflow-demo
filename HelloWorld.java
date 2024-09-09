18079d08df06fc15d33e35dc2982e3082adcd9e5
Gordon <gordon.zhu25@ycdsbk>
2024-09-09 10:17:27 -0400

    Update/test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.*;
+import java.lang.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int[] arr = new int[10];
+        for(int i = 0 ; i < 10;  i++){
+            arr[i] = 123;
+            System.out.println(arr[i]);
+        }
     }
 }
