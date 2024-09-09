47052b35e59bc6a50ce8e578ba04fe708df9e7b4
Gordon <gordon.zhu25@ycdsbk>
2024-09-09 10:36:27 -0400

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ import java.lang.*;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int[] arr = new int[10];
+        int[] arr = new int[100];
         for(int i = 0 ; i < 100;  i++){
             arr[i] = 123;
             System.out.println(arr[i]);
