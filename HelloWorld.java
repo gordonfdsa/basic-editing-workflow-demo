054821fca7f8a862086c49149389ea14a259cc31
Gordon <gordon.zhu25@ycdsbk>
2024-09-09 10:25:06 -0400

    burgrer

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         int[] arr = new int[10];
-        for(int i = 0 ; i < 10;  i++){
+        for(int i = 0 ; i < 100;  i++){
             arr[i] = 123;
             System.out.println(arr[i]);
         }
